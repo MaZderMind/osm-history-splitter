d057dcaddb99d5fa7a17915f8ec4d422a82e05ae
MaZderMind <peter@mazdermind.de>
2011-04-26 23:37:09 +0200

    init & disacard writer, push els to writer

--- cut.hpp
+++ cut.hpp
@@ -1,3 +1,5 @@
+#include <osmium/output/xml.hpp>
+
 class Cut : public Osmium::Handler::Base {
     
 protected:
@@ -5,14 +7,22 @@ protected:
     struct bbox {
         std::string name;
         double x1, y1, x2, y2;
+        Osmium::Output::XML *writer;
     };
     
     std::vector<bbox> bboxes;
     
+    ~Cut() {
+        for(int i=0, l = bboxes.size(); i<l; i++) {
+            delete bboxes[i].writer;
+        }
+    }
+    
 public:
+    
     void addBbox(std::string name, double x1, double y1, double x2, double y2) {
-        bbox bbox = { name, x1, y1, x2, y2 };
-        bboxes.push_back(bbox);
+        bbox b = {name, x1, y1, x2, y2, new Osmium::Output::XML(name.c_str()) };
+        bboxes.push_back(b);
     }
 };
 
--- hardcut.hpp
+++ hardcut.hpp
@@ -6,22 +6,27 @@ public:
     
     void callback_init() {
         printf("hardcut init\n");
-    }
-    
-    void callback_node(Osmium::OSM::Node *node) {
-        for(int i, l = bboxes.size(); i<l; i++) {
+        for(int i = 0, l = bboxes.size(); i<l; i++) {
             printf("\tbbox %s\n", bboxes[i].name.c_str());
         }
-        
-        printf("hardcut node %d v%d, visi: %d\n", node->id, node->version, node->visible);
     }
     
-    void callback_way(Osmium::OSM::Way *way) {
+    void callback_node(Osmium::OSM::Node *e) {
+        printf("hardcut node %d v%d, visi: %d\n", e->id, e->version, e->visible);
+        for(int i = 0, l = bboxes.size(); i<l; i++)
+            bboxes[i].writer->write(e);
+    }
+    
+    void callback_way(Osmium::OSM::Way *e) {
         printf("hardcut way\n");
+        for(int i = 0, l = bboxes.size(); i<l; i++)
+            bboxes[i].writer->write(e);
     }
 
-    void callback_relation(Osmium::OSM::Relation *relation) {
+    void callback_relation(Osmium::OSM::Relation *e) {
         printf("hardcut relation\n");
+        for(int i = 0, l = bboxes.size(); i<l; i++)
+            bboxes[i].writer->write(e);
     }
 
     void callback_final() {
