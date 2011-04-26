c9b1d6f9dfcedb7b73f49e0d44d4337415b9ff89
MaZderMind <peter@mazdermind.de>
2011-04-27 00:47:06 +0200

    enable visible-flag in xml-writer

--- cut.hpp
+++ cut.hpp
@@ -22,6 +22,7 @@ public:
     
     void addBbox(std::string name, double x1, double y1, double x2, double y2) {
         Osmium::Output::XML *writer = new Osmium::Output::XML(name.c_str());
+        writer->writeVisibleAttr = true; // enable visible attribute
         writer->writeBounds(x1, y1, x2, y2);
         
         bbox b = {name, x1, y1, x2, y2, writer };
